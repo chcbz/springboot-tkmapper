b743e671ee24791d3c61086adac35c81c72fb9bb
chcbz <chcbz@sina.com>
2019-08-13 21:43:20 +0800

    增加数据库脚本

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # springboot-tkmapper
 springboot集成mybatis,pageHelper,tkmapper
+
+1、首先导入user.sql到数据库
+2、在IDEA上引入pom.xml
+3、修改application.yml中的数据库连接信息
+4、运行TKApplication
+5、在浏览器打开http://127.0.0.1:8089/swagger-ui.html
---
+++ user.sql
@@ -0,0 +1,38 @@
+﻿/*
+Navicat MySQL Data Transfer
+
+Source Server         : jia-dev
+Source Server Version : 50622
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 50622
+File Encoding         : 65001
+
+Date: 2019-08-13 21:37:50
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` varchar(11) NOT NULL,
+  `name` varchar(20) DEFAULT NULL,
+  `address` varchar(50) DEFAULT NULL,
+  `age` int(3) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `status` int(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', '张三', '路边', '12', '2019-08-13 20:55:49', '2019-08-13 20:55:51', '1');
+INSERT INTO `user` VALUES ('2', '李四', '天桥', '13', '2019-08-13 20:55:55', '2019-08-13 20:55:59', '1');
+INSERT INTO `user` VALUES ('3', '王五', '家里', '14', '2019-08-13 21:19:50', '2019-08-13 21:19:50', '1');
